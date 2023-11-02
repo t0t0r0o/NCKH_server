ac075d90abf7f29d5409e7629ecce88ff696fb48
truong <xuantruongsn2002@gmail.com>
2023-11-02 21:48:31 +0700

    them chuc nang dang nhap va xac thuc

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { createPublicKey, getPublicKeyByName } = require("./publicKeyModel");
+const {
+  createPublicKey,
+  getPublicKeyByName,
+} = require("./models/publicKeyModel");
+
+const {authentication, addUser} = require("./models/userModel");
+const {checkToken} = require("./middleware/checkToken");
 const cors = require("cors");
 
 const app = express();
@@ -13,7 +19,8 @@ const port = 9000;
 
 app.use(bodyParser.json());
 
-app.post("/keys", async (req, res) => {
+
+app.post("/keys",checkToken, async (req, res) => {
   try {
     let name = req.body.name;
     let key = req.body.key;
@@ -23,9 +30,9 @@ app.post("/keys", async (req, res) => {
     }
 
     const keyCreated = await createPublicKey(name, key);
-    console.log(keyCreated);
+
     if (keyCreated.status) {
-      res.status(201).json({ isSuccess: true, data: keyCreated.data });
+      res.status(201).json({ isSuccess: true, data: keyCreated });
     } else {
       res.status(400).json({ isSuccess: false, data: keyCreated });
     }
@@ -37,7 +44,7 @@ app.post("/keys", async (req, res) => {
   }
 });
 
-app.get("/keys/:name", async (req, res) => {
+app.get("/keys/:name",checkToken, async (req, res) => {
   try {
     let name = req.params.name;
 
@@ -48,11 +55,69 @@ app.get("/keys/:name", async (req, res) => {
     const publickey = await getPublicKeyByName(name);
 
     if (publickey) {
-      res.status(200).json({ isSuccess: true, data: publickey.data });
+      res.status(200).json({ isSuccess: true, data: publickey });
     } else {
       res.status(400).json({ isSuccess: false, data: publickey });
     }
   } catch (err) {
+    res.status(500).json({
+      isSuccess: false,
+      error: "Something went wrong!",
+    });
+  }
+});
+
+app.post("/auth", async (req, res) => {
+  try {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if (!username || !password) {
+      res.status(400).json({ isSuccess: false, message: "Missing something" });
+    }
+
+    const auth = await authentication(username, password);
+
+    if(auth.status) {
+      res.status(200).json({isSuccess:true,data: auth.data})
+    } else {
+      res.status(400).json({isSuccess:false,message: "Error"})
+    }
+
+    console.log(auth);
+
+
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({
+      isSuccess: false,
+      error: "Something went wrong!",
+    });
+  }
+});
+
+app.post("/auth/register", async (req, res) => {
+  try {
+    let username = req.body.username;
+    let password = req.body.password;
+    let email = req.body.email;
+
+    if (!username || !password || !email) {
+      res.status(400).json({ isSuccess: false, message: "Missing something" });
+    }
+
+    const user = await addUser(username, password, email);
+
+    if(user.status) {
+      res.status(201).json({isSuccess:true,data: user.data})
+    } else {
+      res.status(400).json({isSuccess:false,message: "Created failure"})
+    }
+
+
+
+  } catch (err) {
+    console.log(err)
     res.status(500).json({
       isSuccess: false,
       error: "Something went wrong!",
